96f6f3d7199d212db3f784b75f9fc6b03deb05ad
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2024-07-15 13:38:32 +0900

    no comment

--- src/winrtspeech/cli/__init__.py
+++ src/winrtspeech/cli/__init__.py
@@ -18,20 +18,20 @@ def find_voice(name):
 
 
 async def save_stream(stream, outfile):
-    reader = DataReader(stream)
-    await reader.LoadAsync(stream.Size)
     buf = [None] * stream.Size
-    reader.ReadBytes(buf)
+    with DataReader(stream) as reader:
+        await reader.LoadAsync(stream.Size)
+        reader.ReadBytes(buf)
     Path(outfile).write_bytes(bytes(buf))
 
 
 async def play_stream(stream):
-    player = MediaPlayer()
-    player.Source = MediaSource.CreateFromStream(stream, stream.ContentType)
-    future = asyncio.get_event_loop().create_future()
-    player.MediaEnded += lambda *_: future.set_result(0)
-    player.Play()
-    await future
+    with MediaPlayer() as player:
+        player.Source = MediaSource.CreateFromStream(stream, stream.ContentType)
+        future = asyncio.get_event_loop().create_future()
+        player.MediaEnded += lambda *_: future.set_result(0)
+        player.Play()
+        await future
 
 
 def command_list(args):
@@ -44,26 +44,25 @@ def command_list(args):
 
 
 async def command_speech(args):
-    synth = SpeechSynthesizer()
+    with SpeechSynthesizer() as synth:
+        if args.voice is not None:
+            synth.Voice = find_voice(args.voice)
 
-    if args.voice is not None:
-        synth.Voice = find_voice(args.voice)
+        if Path(args.message).is_file():
+            message = Path(args.message).read_text(encoding="utf-8")
+        else:
+            message = args.message
 
-    if Path(args.message).is_file():
-        message = Path(args.message).read_text(encoding="utf-8")
-    else:
-        message = args.message
+        if args.ssml:
+            stream = await synth.SynthesizeSsmlToStreamAsync(message)
+        else:
+            stream = await synth.SynthesizeTextToStreamAsync(message)
 
-    if args.ssml:
-        stream = await synth.SynthesizeSsmlToStreamAsync(message)
-    else:
-        stream = await synth.SynthesizeTextToStreamAsync(message)
-
-    if args.out is not None:
-        await save_stream(stream, args.out)
-        return
-
-    await play_stream(stream)
+        with stream:
+            if args.out is not None:
+                await save_stream(stream, args.out)
+            else:
+                await play_stream(stream)
 
 
 async def main_async():
